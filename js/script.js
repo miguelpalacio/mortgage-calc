7b360d0252439a5cc2855c93572c6e7466545f73
Miguel Palacio <palaciodelgado@gmail.com>
2018-07-08 18:59:14 -0500

    Perform calculations nad show results

--- js/script.js
+++ js/script.js
@@ -3,9 +3,26 @@ function onCalculateMortgage() {
 		return;
 	}
 
-	renameCalculateButton();
+	const result = {
+		principleAndInterest: 0,
+		tax: 0,
+		insurance: 0,
+		totalPayment: 0
+	};
+
+	calculatePrincipleAndInterest(result);
+	calculateTax(result);
+	calculateInsurance(result);
+	calculateMonthlyPayment(result);
+
+	updateResultLabels(result);
 	darkenResultValueLabels();
+	renameCalculateButton();
 }
+
+// -------------------------------------
+// Validity of entered values
+// -------------------------------------
 
 function isFormValid() {
 	let formIsValid = true;
@@ -22,34 +39,17 @@ function highlightCurrencyInputErrors (formIsValid, fieldId) {
 	const $fieldElemError = document.getElementById(fieldId + 'Error');
 
 	if ($fieldElem.value.length === 0 || $fieldElem.value === '.') {
+		// Invalid input
 		$fieldElem.classList.add('error');
 		$fieldElemError.classList.remove('field-error-hidden');
 		formIsValid = false;
 	} else {
+		// Valid input
 		$fieldElem.classList.remove('error');
 		$fieldElemError.classList.add('field-error-hidden');
 	}
 	
 	return formIsValid;
-}
-
-function renameCalculateButton() {
-	document.getElementById('calculateButton').value = 'RECALCULATE';
-}
-
-// Remove opacity property of result-label elements
-function darkenResultValueLabels() {
-	$resultValueElems = document.getElementsByClassName('result-value');
-	for (let i = 0, l = $resultValueElems.length; i < l; i++) {
-		const elem = $resultValueElems.item(i);
-		elem.classList.remove('no-value');
-	}
-}
-
-// Live updates of text fields beside the sliders
-function updateSliderTextField(slider, value) {
-	$textField = slider.nextElementSibling.nextElementSibling;
-	$textField.value = value;
 }
 
 // Do not allow non-numeric characters (only allow ONE '.' for decimals)
@@ -67,5 +67,72 @@ function validateCurrencyInput(textField, event) {
 	if ((event.charCode === 46 && pointPresent) ||
 		(event.charCode !== 46 && (event.charCode < 48 || event.charCode > 57))) {
 		event.preventDefault();
+	}
+}
+
+// -------------------------------------
+// Calculations
+// -------------------------------------
+
+function calculatePrincipleAndInterest (result) {
+	const rateOfInterest = Number(document.getElementById('rateOfInterest').value);
+	const loanAmount = Number(document.getElementById('loanAmount').value);
+	const yearsOfMortgage = Number(document.getElementById('yearsOfMortage').value);
+
+	result.principleAndInterest = ((rateOfInterest / 100) / 12) * loanAmount /
+		(1 - Math.pow(1 + ((rateOfInterest / 100) / 12), -yearsOfMortgage * 12));
+}
+
+function calculateTax (result) {
+	const annualTax = Number(document.getElementById('annualTax').value);
+
+	result.tax = annualTax / 12;
+}
+
+function calculateInsurance (result) {
+	const annualInsurance = Number(document.getElementById('annualInsurance').value);
+
+	result.insurance = annualInsurance / 12;
+}
+
+function calculateMonthlyPayment (result) {
+	result.totalPayment = result.principleAndInterest + result.tax + result.insurance;
+}
+
+// -------------------------------------
+// Update labels in calculator-results
+// -------------------------------------
+
+function updateResultLabels (result) {
+	for (const key in result) {
+		const $label = document.getElementById(key + 'Label');
+		const value = result[key];
+
+		const valueHasDecimals = value !== parseInt(value);
+
+		$label.innerHTML = '$ ' + (valueHasDecimals ? value.toFixed(2) : value);
+	}
+}
+
+// -------------------------------------
+// Misc
+// -------------------------------------
+
+// Live updates of text fields beside the sliders
+function updateSliderTextField(slider, value) {
+	$textField = slider.nextElementSibling.nextElementSibling;
+	$textField.value = value;
+}
+
+function renameCalculateButton() {
+	document.getElementById('calculateButton').value = 'RECALCULATE';
+}
+
+// Remove opacity property of result-label elements
+function darkenResultValueLabels() {
+	$resultValueElems = document.getElementsByClassName('result-value');
+	for (let i = 0, l = $resultValueElems.length; i < l; i++) {
+		const elem = $resultValueElems.item(i);
+		elem.classList.remove('no-value');
 	}
 }
